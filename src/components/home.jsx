38e9928db71358e4cf9f39b89e8df0cb80eee467
Nejla El morabet <elmorabet.n@hotmail.com>
2023-12-22 13:14:53 +0100

    style mis a jour pour section about

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -74,12 +74,14 @@ const About = () => {
   useScrollAnimation('.animate');
   return (
     <div className="about">
+      <img className='aboutImage animate' src='/images/aboutus-image.png' alt="About Us" />
       <div className="about-content">
+        
         <h2 className='animate'>About Us</h2>
-        <img className='animate' src='/images/aboutus-image.png' alt="About Us" />
         <p className='animate'>
         Chez TALE, nous façonnons des narrations publicitaires uniques en collaborant étroitement avec nos partenaires pour cerner leurs aspirations spécifiques. Nous nous spécialisons dans la création de stratégies publicitaires sur mesure, méticuleusement adaptées à chaque plateforme sociale. Notre approche repose sur la créativité et la précision, fusionnant habilement images, vidéos et messages publicitaires percutants. Nous nous efforçons de captiver et de convertir efficacement les utilisateurs, façonnant ainsi des campagnes qui transcendent les attentes et propulsent nos clients vers de nouveaux sommets.</p>
-        <img src="/images/TALE-signature.png" alt="" />
+        <img className='signature' src="/images/TALE-signature.png" alt="" />
+
       </div>
     </div>
   );
