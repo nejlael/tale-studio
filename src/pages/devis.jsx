f07912a10fba11c82323b9f5c5685b172f77cf92
SimplyV <gillian@mfmdigital.com>
2023-12-22 13:41:03 +0100

    Ajout page contact + devis

---
+++ src/pages/devis.jsx
@@ -0,0 +1,38 @@
+import Form from "../components/form";
+import { FaPhone, FaMail }from 'react-icons/fa';
+import { IoLocationSharp } from "react-icons/io5";
+
+const Devis = () => {
+  return(
+    <section className="ask-section">
+      <div className="ask-section-cover">
+
+      </div>
+      <div className="ask-section-content">
+        <Form type={'devis'} />
+        <div className="ask-section-content-title">
+          <h2> CONTACTEZ <br /> NOUS !</h2>
+        </div>
+        <div className="ask-section-content-infos">
+          <p><b> Nous appeler : </b></p>
+          <div>
+            <FaPhone />
+            <a href="tel:0471574096"> 0471574096 </a>
+          </div>
+          <p><b> Où nous trouver ? </b></p>
+          <div>
+            <IoLocationSharp />
+            <p> Rue de Moorslede 08 <br /> 1020, Laeken</p>
+          </div>
+          <p><b> Nous contacter par mail : </b></p>
+          <div>
+            <FaMail />
+            <a href="mailto:info@tale-studio.com"> info@tale-studio.com </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Devis;
